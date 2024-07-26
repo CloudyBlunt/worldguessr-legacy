2e67c66cad68c860a0a269abbc133042d0f5c787
TheUltimateSoyCode <v1ss0nd@proton.me>
2024-07-26 23:09:25 +0600

    made a field for notes, i thought it would be helpful

--- components/navbar.js
+++ components/navbar.js
@@ -1,16 +1,16 @@
-export default function Navbar({mapShown, fullReset, playingMultiplayer,}) {
-return (
+export default function Navbar({ mapShown, fullReset, playingMultiplayer }) {
+  return (
     <div className={`top ${mapShown ? 'hideOnMobile' : ''}`}>
       <div className="topItem navbar">
         <div>
-          <a id="logo" alt="worldguessr logo" onClick={() => {
-          }} style={{ cursor: "pointer" }}>
+          <a id="logo" alt="worldguessr logo" onClick={() => { }} style={{ cursor: "pointer" }}>
             <img id="icon" src="/logo.png" alt="WorldGuessr logo" />
           </a>
           {playingMultiplayer ? <p style={{ color: 'white' }}></p> : (
-            <button className="navButton" onClick={() => {if (!playingMultiplayer) fullReset();}}>New drop</button>
+            <button className="navButton" onClick={() => { if (!playingMultiplayer) fullReset(); }}>New drop</button>
           )}
-        </div>        
+          <input type="text" />
+        </div>
       </div>
     </div>
   );
